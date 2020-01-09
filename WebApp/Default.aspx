47bc7944f104d9524bb36b7158f5a66936c4d7e0
Oleg Shapoval <Olegx4@ukr.net>
2020-01-09 14:33:11 +0200

    calculator

---
+++ WebApp/Default.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calculator</title>
+    <style>
+        .container span {
+            display: inline-block;
+            width:92px;
+        }
+    </style>
+</head>
+
+<body style="height: 174px">
+    <form id="form1" runat="server">
+    <div>
+    <h2>Калькулятор прожитого часу</h2>
+    </div>
+        Поточний час:
+        <asp:Label ID="outputLabel" runat="server" OnLoad="Page_Load" Text="Label"></asp:Label>
+        &nbsp;<p>
+            &nbsp;</p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Введіть ваші дані ПІБ"></asp:Label>
+        </p>
+        <p>
+            <asp:TextBox ID="FioTxtBox" runat="server" Width="287px"></asp:TextBox>
+        </p>
+        <p>
+            Виберіть дату народження:
+        </p>
+ 
+        <div class="container">
+            <asp:TextBox ID="TextBoxDate" runat="server" MaxLength="2" Width="277px" TextMode="Date" AutoPostBack="True" OnTextChanged="TextBoxDate_TextChanged"></asp:TextBox>
+            <%--<span>Ден</span>
+            <span>Місяць</span>
+            <span>Рік (YYYY)</span>--%>&nbsp;Обрахувати скільки часу ви прожили:
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Go!" />
+            <br />
+        </div>
+            
+
+        <p>
+            <asp:Label ID="outputTextTimeNow" runat="server"></asp:Label>
+        </p>
+        <asp:TextBox ID="TextBoxRecords" runat="server" Height="125px" ReadOnly="True" TextMode="MultiLine" Width="568px" Wrap="False"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ WebApp/Default.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            outputLabel.Text = DateTime.Now.ToLongTimeString();
+            
+            //TextBoxDate.TextChanged += new EventHandler(TextBoxDay_TextChanged);
+            //TextBoxMonth.TextChanged += new EventHandler(TextBoxDay_TextChanged);
+        }
+
+        //protected void TextBoxDay_TextChanged(object sender, EventArgs e)
+        //{
+        //    if (TextBoxDate.Text.Length == TextBoxDate.MaxLength)
+        //    {
+        //        TextBoxMonth.Focus();
+        //    }
+        //}
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(FioTxtBox.Text != "")
+                {
+                    DateTime begin = Convert.ToDateTime(TextBoxDate.Text);
+                    DateTime end = DateTime.Now;
+                    if (begin < end)
+                    {
+                        outputTextTimeNow.ForeColor = System.Drawing.Color.Black;
+                        TimeSpan res = end.Subtract(begin);
+                        string curRes = ("Прожито днів: " + res.Days +
+                                                    " Годин: " + res.Hours +
+                                                    " Хвилин: " + res.Minutes +
+                                                    " Секунд: " + res.Seconds);
+                        outputTextTimeNow.Text = curRes;
+                        TextBoxRecords.Text += FioTxtBox.Text + " " + curRes + ";\n";
+                    }
+                    else
+                    {
+                        outputTextTimeNow.ForeColor = System.Drawing.Color.Red;
+                        outputTextTimeNow.Text = "Дата вашого народження не може бути у майбутньому часі!";
+                    }
+                }
+                else
+                {
+                    outputTextTimeNow.ForeColor = System.Drawing.Color.Red;
+                    outputTextTimeNow.Text = "Поле ПІБ не має бути пустим.";
+                }           
+            }
+            catch
+            {
+                TextBoxDate.Text = String.Format("{0:dd/MM/yyyy}", DateTime.Now); ;
+                outputTextTimeNow.ForeColor = System.Drawing.Color.Red;
+                outputTextTimeNow.Text = "Дата вашого народження не може бути пустою!";
+            }
+            
+
+            
+
+
+            //Console.WriteLine(res);
+        }
+
+        protected void TextBoxDate_TextChanged(object sender, EventArgs e)
+        {
+            outputTextTimeNow.Text = "";
+           
+        }
+    }
+}
